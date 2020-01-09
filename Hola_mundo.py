2318b4607b307367eb8f1313a7e0d56492906ce3
Lida Patricia <54218446+Lida21-pato@users.noreply.github.com>
2020-01-08 23:17:06 -0500

    Add files via upload

---
+++ Hola_mundo.py
@@ -0,0 +1,268 @@
+# 3.HOLA MUNDO
+'''
+print("Hola mundo")
+print("Mi nombre es Lyda Patricia Lara Dimate")
+
+
+# 4.ASIGNACION DE VALORES
+numero = 10.25
+
+print(numero)
+
+print(type(numero))
+
+cadena = "Lyda esta 'estudiando'"
+
+print(cadena)
+
+print(type(cadena))
+
+valor= True
+
+print(valor)
+print(type(valor))
+
+# 5. COMENTARIOS
+Nos muestra que los comentarios largos van dentro de 3 comillas y los cortos con nmumeral y al ser ejecutado el programa no pasa nada
+
+
+print("OPERACIONES ARITMETICAS")
+num1 = 10
+num2 = 6
+suma = num1+num2
+
+print(suma)
+
+num1 = 10
+num2 = 6.8
+resultado = (num1+num2) * 10 / 6
+
+print("el resultado es :",resultado)
+
+# 6. OPERADORES ARITMETICOS
+
+print("suma")
+
+num1 = 20
+num2 = 5
+
+resultado = num1+num2
+
+print(resultado)
+
+print("resta")
+
+num1 = 20
+num2 = 5
+
+resultado = num1-num2
+
+print(resultado)
+
+print("multiplicacion")
+
+num1 = 20
+num2 = 5
+
+resultado = num1*num2
+
+print(resultado)
+
+print("division")
+
+num1 = 20
+num2 = 5
+
+resultado = num1/num2
+
+print(resultado)
+
+print("modulo")
+
+num1 = 20
+num2 = 5
+
+resultado = num1%num2
+
+print(resultado)
+
+print("Exponenciacion")
+
+num1 = 2
+num2 = 5
+
+resultado = num1**num2
+
+print(resultado)
+
+print("EJEMPLO")
+
+resultado = 3**3 * (13/5 - (2*4))
+
+print(resultado)
+
+# 7.OPERADORES RELACIONALES
+
+print ("Mayor que ")
+resultado = 10 > 20
+
+print(resultado)
+
+print ("Menor que ")
+resultado = 10 < 20
+
+print(resultado)
+
+print ("Igual que ")
+resultado = 10 == 20
+
+print(resultado)
+
+print (" Mayor o Igual que ")
+resultado = 10 >= 20
+
+print(resultado)
+
+print (" Menor o Igual que ")
+resultado = 10 <= 20
+
+print(resultado)
+
+print (" diferente que ")
+resultado = 10 != 10
+
+print(resultado)
+
+
+
+# 8. OPERADORES LOGICOS
+
+a = 10
+b = 15
+c = 20
+
+resultado = ((a>b) and (b<c))
+
+print(resultado)
+
+a = 10
+b = 15
+c = 20
+
+resultado = ((a>b) or (b<c))
+
+print(resultado)
+
+a = 10
+b = 15
+c = 20
+
+resultado = not((a>b) or (b<c))
+
+print(resultado)
+
+
+
+# 9.OPERADORES DE ASIGNACION
+
+a = 0
+
+a += 5 # suma en asigancion
+a -= 2 # resta en asigancion
+a *= 3 # multiplicacion en asigancion
+a /= 3 # division en asigancion
+a **= 2 # potencia en asigancion
+a %= 2 # modulo en asigancion
+
+print(a)
+
+
+
+# 10. SALIDAS
+
+nombre = "Lyda"
+edad = 30
+
+print("Hola",nombre,"tienes",edad,"años")
+
+print("Hola {} tienes {} años".format(nombre,edad))
+
+print(f"Hola {nombre} tienes {edad} años")
+
+
+
+# 11.ENTRADAS DE DATOS
+
+nombre = input("Digite su nombre:")
+
+print(f"Hola {nombre}") # Asi se guardan los datos de tipo cadena
+
+numero = int(input("Digite un numero:"))
+
+print(f"el numero es {numero +1}")
+
+
+
+# 12. FUNCIONES INTEGRADAS
+
+n = float("10.8")
+
+print(n)
+
+n = str("10")
+
+print(n)
+
+n = str("10.98")
+
+print(n)
+
+n = bin(10)
+
+print(n)
+
+n = hex(10)
+
+print(n)
+
+n = int("0b1010",2)
+
+print(n)
+
+n = int("0xa",16)
+
+print(n)
+
+n = abs(-8)
+
+print(n)
+
+n = round(10.58)
+
+print(n)
+
+n = len("Lyda")
+
+print(n)
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
